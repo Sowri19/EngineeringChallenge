0ae3111ec898bba1b66ca18f241513ec7dabbaaa
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2023-12-30 01:14:56 -0500

    Refactor tab layout and add authentication logic

--- native-app/app/(tabs)/_layout.tsx
+++ native-app/app/(tabs)/_layout.tsx
@@ -1,40 +1,79 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import {Link, Tabs} from 'expo-router';
-import {Pressable, useColorScheme} from 'react-native';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs } from "expo-router";
+import { Pressable, useColorScheme } from "react-native";
+import Colors from "../../constants/Colors";
 
-import Colors from '../../constants/Colors';
-
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { loginSuccess, logout } from "../../redux/slices/authSlice";
+import { saveToken, deleteToken } from "../../utils/secureStoreUtils";
+import Login from "../../module/login/index";
+import { RootState } from "../../types/states";
+import { authenticateUser } from "../../utils/authentication";
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof FontAwesome>["name"];
   color: string;
 }) {
-  return <FontAwesome size={28} style={{marginBottom: -3}} {...props} />;
+  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
-  return (
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+
+  // Dummy login check
+  const handleLogin = async () => {
+    // Replace this with your actual login logic
+    const token = await authenticateUser(email, password); // Implement this
+    if (token) {
+      await saveToken(token);
+      dispatch(loginSuccess({ email }));
+    } else {
+      // Handle login failure
+    }
+  };
+
+  const handleLogout = async () => {
+    await deleteToken();
+    dispatch(logout());
+  };
+
+  return !isLoggedIn ? (
+    <Login
+      onPress={handleLogin}
+      setEmail={setEmail}
+      password={password}
+      email={email}
+      setPassword={setPassword}
+    />
+  ) : (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
       }}
     >
       <Tabs.Screen
-        name='index'
+        name="index"
         options={{
-          title: 'Machine State',
-          tabBarIcon: ({color}) => <TabBarIcon name='list-ul' color={color} />,
+          title: "Machine State",
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="list-ul" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
-        name='two'
+        name="two"
         options={{
-          title: 'Log Part',
-          tabBarIcon: ({color}) => <TabBarIcon name='edit' color={color} />,
+          title: "Log Part",
+          tabBarIcon: ({ color }) => <TabBarIcon name="edit" color={color} />,
         }}
       />
     </Tabs>
--- native-app/app/_layout.tsx
+++ native-app/app/_layout.tsx
@@ -1,18 +1,29 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
-import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
+import React, { useEffect } from "react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { useDispatch } from "react-redux";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useColorScheme } from "react-native";
+import { getToken } from "../utils/secureStoreUtils";
+import { loginSuccess, logout } from "../redux/slices/authSlice";
+import { decodeToken } from "../utils/authentication";
+import { store, persistor } from "../redux/store";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  // Ensure that reloading on '/modal' keeps a back button present.
+  initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -20,11 +31,10 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
     if (error) throw error;
   }, [error]);
@@ -39,17 +49,38 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <RootLayoutNav />
+      </PersistGate>
+    </Provider>
+  );
 }
 
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const checkAuthState = async () => {
+      const token = await getToken();
+      if (token) {
+        const userEmail = decodeToken(token);
+        dispatch(loginSuccess({ email: userEmail }));
+      } else {
+        dispatch(logout());
+      }
+    };
+
+    checkAuthState();
+  }, [dispatch]);
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
       </Stack>
     </ThemeProvider>
   );
