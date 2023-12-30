4a81e36fc140685b84006742ad32209e3b1508e3
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2023-12-30 17:43:57 -0500

    Add Firebase configuration and write machine health data to Firestore

--- backend/app.ts
+++ backend/app.ts
@@ -1,22 +1,31 @@
-import express, {Request, Response} from 'express';
-import {getMachineHealth} from './machineHealth';
+// Import necessary modules
+import express, { Request, Response } from "express";
+import admin from "./firebaseConfig"; // Import admin from firebaseConfig
+import { getMachineHealth } from "./machineHealth";
 
 const app = express();
 const port = 3001;
 
-// Middleware to parse JSON request bodies
-app.use(express.json());
+app.use(express.json()); // Middleware for JSON request parsing
 
-// Endpoint to get machine health score
-app.post('/machine-health', (req: Request, res: Response) => {
+app.post("/machine-health", async (req: Request, res: Response) => {
   const result = getMachineHealth(req);
   if (result.error) {
     res.status(400).json(result);
   } else {
-    res.json(result);
+    const db = admin.firestore();
+    const docRef = db.collection("machineHealthRecords").doc();
+    try {
+      await docRef.set(result);
+      console.log("Machine health data written with ID: ", docRef.id);
+      res.json(result);
+    } catch (error) {
+      console.error("Error writing machine health data to Firebase:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   }
 });
 
 app.listen(port, () => {
-  console.log(`API is listening at http://localhost:${port}`);
+  console.log(`Server is listening at http://localhost:${port}`);
 });
