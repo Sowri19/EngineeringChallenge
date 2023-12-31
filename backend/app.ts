e02678174c888bbdd81af7483d0a10ba6e52edd8
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2023-12-31 01:54:37 -0500

    Add timestamp to machine health data

--- backend/app.ts
+++ backend/app.ts
@@ -1,4 +1,3 @@
-// Import necessary modules
 import express, { Request, Response } from "express";
 import admin from "./firebaseConfig"; // Import admin from firebaseConfig
 import { getMachineHealth } from "./machineHealth";
@@ -10,15 +9,23 @@ app.use(express.json()); // Middleware for JSON request parsing
 
 app.post("/machine-health", async (req: Request, res: Response) => {
   const result = getMachineHealth(req);
+  const timestamp = new Date().toISOString(); // Get current timestamp
+
   if (result.error) {
     res.status(400).json(result);
   } else {
     const db = admin.firestore();
     const docRef = db.collection("machineHealthRecords").doc();
     try {
-      await docRef.set(result);
-      console.log("Machine health data written with ID: ", docRef.id);
-      res.json(result);
+      // Include the timestamp in the data being saved
+      await docRef.set({ ...result, timestamp });
+      console.log(
+        "Machine health data written with ID: ",
+        docRef.id,
+        " at ",
+        timestamp
+      );
+      res.json({ ...result, timestamp });
     } catch (error) {
       console.error("Error writing machine health data to Firebase:", error);
       res.status(500).json({ error: "Internal Server Error" });
